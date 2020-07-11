28052a12c53a67a9370ae91de4d24b410b51c3c7
Bruno G <54629344+brgian@users.noreply.github.com>
2020-07-11 18:20:26 -0300

    Initial commit

---
+++ NetCore.Template.Repositories/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace NetCore.Template.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        T GetById(long id);
+        IQueryable<T> GetAll(Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null);
+        IQueryable<T> Search(Expression<Func<T, bool>> predicate, Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null);
+        T FindSingle(Expression<Func<T, bool>> predicate, Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null);
+        T Add(T entity);
+        T Update(T entity);
+        void Remove(long entity);
+    }
+}
